ea0a797293ef7e58c4eac0ed9f62532b2bb67dc5
jonosuave <jonathan.duncan@journeyteam.com>
2023-06-12 15:22:37 -0600

    first commit

---
+++ config.ts
@@ -0,0 +1,9 @@
+const config = {
+	botId: process.env.BOT_ID,
+	botPassword: process.env.BOT_PASSWORD,
+	openAIKey: process.env.SECRET_OPENAI_API_KEY,
+	azureOpenAIEndpoint: process.env.AZURE_OPENAI_ENDPOINT,
+	azureAIKey: process.env.SECRET_AZUREAI_API_KEY,
+};
+
+export default config;
